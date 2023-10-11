24ad391b66256ae77a95cc84856d7c83a5ac28e7
Henrique <yunohenrique2@gmail.com>
2023-10-10 21:37:49 -0300

    first commit

---
+++ Course/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Course {
+    internal class Program {
+        static void Main(string[] args) {
+
+            double a;
+            int b;
+
+            a = 5.1;
+            b = (int)a;
+
+            Console.WriteLine(b);
+        }
+    }
+}
